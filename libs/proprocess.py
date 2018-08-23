f97cf10ea1b171b7c6e7a779e90bb981e931411c
Sheldon <Sheldon Hur>
2018-08-23 17:25:01 +0400

    add preprocess handle set

---
+++ libs/proprocess.py
@@ -0,0 +1,21 @@
+class process(object):
+    def __init__(self,dict):
+        self.__dict__.update(dict)
+
+    def save(self,file_path):
+        pass
+    
+    def load(self,file_path):
+        pass
+
+    def foreward(self,im):
+        pass
+    
+    def backward(self.im):
+        pass
+        
+
+
+class process_collecction(object):
+    def __init__(self):
+        pass
